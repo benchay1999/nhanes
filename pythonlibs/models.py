c087344f25a1e2a368bd775e9ca6c6966467324a
benchay1999 <benchay@kaist.ac.kr>
2022-04-21 08:05:58 +0000

    NHANES 2009-2018 preprocessed data

--- pythonlibs/models.py
+++ pythonlibs/models.py
@@ -1,33 +1,3 @@
-import torch
-import torch.nn as nn
-class Classifier(nn.Module):
-    def __init__(self, n_layers, n_inputs, n_hidden_units, n_classes):
-        super(Classifier, self).__init__()
-        layers = []
-
-        if n_layers == 1: # Logistic Regression
-            if n_classes > 2:
-                layers.append(nn.Linear(n_inputs, n_classes))
-            else:
-                layers.append(nn.Linear(n_inputs, 1))
-        
-        else : #Deep  Neural Network
-            layers.append(nn.Linear(n_inputs, n_hidden_units))
-            layers.append(nn.ReLU())
-            for i in range(n_layers-2):
-                layers.append(nn.Linear(n_hidden_units, n_hidden_units))
-                layers.append(nn.ReLU())
-            
-            if n_classes > 2:
-                layers.append(nn.Linear(n_hidden_units, n_classes))
-            else:
-                layers.append(nn.Linear(n_hidden_units, 1))
-        
-        if n_classes > 2:
-            layers.append(nn.Softmax(dim=1))
-        else:
-            layers.append(nn.Sigmoid())
-        self.layers = nn.Sequential(*layers)
-
-    def forward(self, x):
-        return self.layers(x)
+version https://git-lfs.github.com/spec/v1
+oid sha256:9577150014489ab8cb97195e89719a64b1a65e813f7ff194bce878c0be070e45
+size 1136
